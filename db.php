2b5a8cfb80622545adccb67eddeb2ac6cdf10455
Ellia <hlccntayuni@gmail.com>
2026-06-02 06:01:27 +0800

    update again

--- db.php
+++ db.php
@@ -1,8 +1,17 @@
 <?php
-$hostname = 'sql313.infinityfree.com';
-$username = 'rootif0_42070499';
-$password = 'woyNbfp5xk';
-$database = 'if0_42070499_db_trinova';
 
-$conn = mysqli_connect($hostname, $username, $password, $database) or die ('Gagal menyambungkan ke database');   
+$hostname = getenv('mysql.railway.internal');
+$username = getenv('root');
+$password = getenv('cQDuLQbdZxuuifRYNuAAhPISXRhtKIlk');
+$dbname   = getenv('railway');
+$port     = getenv('3306');
+
+$conn = mysqli_connect(
+    $hostname,
+    $username,
+    $password,
+    $dbname,
+    (int)$port
+) or die('Gagal terhubung ke database');
+
 ?>
